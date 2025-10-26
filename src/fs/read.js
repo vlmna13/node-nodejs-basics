382a87a575340de38fc83927377bc34271870515
vlmna13 <vlmna13@gmail.com>
2025-10-26 16:42:56 +0300

    implement function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  try {
+    const content = await fs.readFile(filePath, 'utf-8');
+    console.log(content)
+  } catch(error) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await read();
+(async ()=>{
+  try {
+    await read();
+  } catch(error) {
+    console.log(error.message);
+  }
+})();
