e3d8d4980addbec846fedd9bd27ff2ebc4e790b4
vlmna13 <vlmna13@gmail.com>
2025-10-26 17:33:24 +0300

    implement function read in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const information = path.join(__dirname, "files", "fileToRead.txt");
+  createReadStream(information).pipe(process.stdout);
 };
 
 await read();
