6852fa87527cd84630ffebffa57cf0351e3bbd01
vlmna13 <vlmna13@gmail.com>
2025-10-26 20:53:45 +0300

    implement parseArgs in cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+    const args = process.argv.slice(2);
+    const result = [];
+    let i = 0;
+
+    while (i < args.length) {
+        const propName = args[i].replace('--', '');
+        const value = args[i + 1];
+        result.push(`${propName} is ${value}`);
+        i += 2;
+    }
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
