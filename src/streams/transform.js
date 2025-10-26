22c2892f207479923e568134ea7a49b9b63f82bd
vlmna13 <vlmna13@gmail.com>
2025-10-26 23:15:12 +0300

    implement function transfor in streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-  // Write your code here
+  class ReverseTransform extends Transform {
+    _transform(chunk, encoding, callback) {
+      const reversed = chunk.toString().split("").reverse().join("");
+      this.push(reversed);
+      callback();
+    }
+  }
+
+  await pipeline(process.stdin, new ReverseTransform(), process.stdout);
 };
 
 await transform();
