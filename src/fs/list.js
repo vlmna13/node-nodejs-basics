1f7c7bf19ec14a5cfd6251aba42585301c4cdb97
vlmna13 <vlmna13@gmail.com>
2025-10-26 16:33:28 +0300

    implement function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-  // Write your code here
+  const folderPath = path.join(__dirname, "files");
+  try {
+    const files = await fs.readdir(folderPath);
+    console.log(files);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
-await list();
+(async () => {
+  try {
+    await list();
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
