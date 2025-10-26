d707511eb3376c964749029e4a4143ec4c064a48
vlmna13 <vlmna13@gmail.com>
2025-10-26 19:32:46 +0300

    implement function calcHash in hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { pipeline } from 'stream/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const calculateHash = async () => {
-  // Write your code here
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    
+    try {
+        const hash = createHash('sha256');
+        const readStream = createReadStream(filePath);
+                await pipeline(
+            readStream,
+            hash
+        );
+        
+        const hexHash = hash.digest('hex');
+        console.log(hexHash);
+        return hexHash;
+    } catch (error) {
+        console.error('Error calculating hash:', error.message);
+        throw error;
+    }
 };
 
 await calculateHash();
