ac1352517f8df8f93b70519290e8d6e4be04e22c
vlmna13 <vlmna13@gmail.com>
2025-10-26 17:49:09 +0300

    implement function write in streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { createWriteStream } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const file = path.join(__dirname, "files", "fileToWrite.txt");
+  process.stdin.pipe(createWriteStream(file));
 };
 
 await write();
